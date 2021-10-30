2a7c17dd758796c94f0856d07f112992c0177620
Brandon Miller <brandonkmiller@protonmail.com>
2021-10-29 22:39:22 -0400

    Added flags to vizzyviz.py script
    By default it now prints the heap summary and --mem-usage is used to
    generate the heap memory usage visualization. This commit also formats
    the code so that it's pylint compliant and adds docstrings

--- script/vizzyviz.py
+++ script/vizzyviz.py
@@ -1,16 +1,28 @@
+"""Script for analyzing and visualizing vizzy heap trace files
+"""
+
 import argparse
-import csv
 from collections import OrderedDict
 from bokeh.plotting import figure, show
 
-TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+TOOLS = 'pan,wheel_zoom,box_zoom,reset,save'
 
-def heap_summary(dataset):
+def heap_summary(dataset: OrderedDict):
+    """Analyzes the dataset and prints a heap summary
+
+    Iterates through the ordered dictionary and keeps track of allocations and frees in order.
+    Prints report on number of allocations, number of frees, total bytes allocated, and memory
+    that hasn't been freed on exit.
+
+    Args:
+      dataset: Ordered dictionary containing data parsed from vizzy trace file
+    """
+
     allocnum = 0
     freenum = 0
     allocs = OrderedDict()
     total_bytes_allocated = 0
-    for time, data in dataset.items():
+    for _, data in dataset.items():
         if data['function'] == 'free':
             freenum += 1
             if data['address'] in allocs:
@@ -32,17 +44,18 @@ def heap_summary(dataset):
     print('''HEAP SUMMARY:
     in use at exit   : {} bytes in {} blocks
     total heap usage : {} allocs, {} frees, {} bytes allocated'''.format(
-        in_use_bytes, in_use_blocks, allocnum, freenum, total_bytes_allocated)
-    )
+        in_use_bytes, in_use_blocks, allocnum, freenum, total_bytes_allocated))
 
-def user_address_range():
-    user_range = []
-    for i in range(0, 0x800000000000, 0x20000000000):
-        user_range.append(i)
+def viz_heap_usage_over_time(dataset: OrderedDict):
+    """Generate a line graph SVG showing heap memory consumption over time
 
-    return user_range
+    Processes dataset from the vizzy trace file and creates a SVG line graph that shows heap memory
+    consumption over time. Displays the SVG in the user's default browser.
 
-def viz_heap_usage_over_time(dataset):
+    Args:
+      dataset: Ordered dictionary containing data parsed from vizzy trace file
+    """
+
     curr_allocs = OrderedDict()
     curr_memusage = 0
     times = []
@@ -58,19 +71,29 @@ def viz_heap_usage_over_time(dataset):
 
         times.append(time)
         memusage.append(curr_memusage/1024.0)
-    p = figure(title="Memory Usage over Time", x_axis_label='Time (seconds)', y_axis_label='Memory (kB)', tools=TOOLS)
-    p.line(times, memusage, line_width=0.5)
-    show(p)
+    fig = figure(title="Memory Usage over Time", x_axis_label='Time (seconds)',
+                 y_axis_label='Memory (kB)', tools=TOOLS)
+    fig.line(times, memusage, line_width=0.5)
+    show(fig)
 
-def parse_vizzy_file(tracefile):
-    """Iterate through CSV rows
+def parse_vizzy_file(tracefile: str) -> OrderedDict:
+    """Iterate through trace file lines and parse data into an ordered dictionary
+
+    Converts the nanoseconds to seconds and adds to seconds field to get absolute time of traced
+    event in seconds (as a float)
+
+    Args:
+      tracefile: Path to vizzy trace file
+
+    Returns:
+      Ordered dictionary of parsed data
     """
 
     dataset = OrderedDict()
-    with open(tracefile, 'r') as f:
-        linenum = 0
+    with open(tracefile, 'r') as _file:
+        first_line = True
         starttime = 0
-        for line in f.readlines():
+        for line in _file.readlines():
             line = line.strip()
             elements = line.split(',')
 
@@ -85,8 +108,9 @@ def parse_vizzy_file(tracefile):
                 size = int(elements[4])
 
             reltime = 0
-            if linenum == 0:
+            if first_line:
                 starttime = abstime
+                first_line = False
             else:
                 reltime = abstime-starttime
 
@@ -96,16 +120,16 @@ def parse_vizzy_file(tracefile):
                 size=size,
             )
 
-            linenum += 1
-
     return dataset
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
     """Parse command line arguments
     """
 
     parser = argparse.ArgumentParser()
     parser.add_argument('tracefile', help='File path to vizzy heap trace file')
+    parser.add_argument('--mem-usage', action='store_true',
+                        help='Visualize heap memory usage over time')
     return parser.parse_args()
 
 def main():
@@ -114,8 +138,11 @@ def main():
 
     args = parse_args()
     dataset = parse_vizzy_file(args.tracefile)
-    heap_summary(dataset)
-    viz_heap_usage_over_time(dataset)
+    if args.mem_usage:
+        viz_heap_usage_over_time(dataset)
+    else:
+        heap_summary(dataset)
+
 
 if __name__ == '__main__':
     main()
